2957d4436071129606726bb60d9599afdd3ec33e
beemi <beemi.raja@gmail.com>
2021-03-12 22:43:28 +0000

    Terminal output plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,16 @@
 
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
 
 module.exports = (on, config) => {
 
     // allure report plugin
     allureWriter(on, config);
 
-    // Cypress terminal output
-    require('cypress-terminal-report/src/installLogsPrinter')(on);
+    // Terminal output plugin
+    installLogsPrinter(on, {
+        // onFail, always, never
+        printLogsToConsole: "always",
+        routeTrimLength: 500
+    });
 }
