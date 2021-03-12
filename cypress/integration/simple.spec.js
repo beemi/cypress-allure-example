d053bfe504833925db2a6498ed53e8a5975cd5d4
beemi <beemi.raja@gmail.com>
2021-03-12 13:26:45 +0000

    Sudo install yes added

--- cypress/integration/simple.spec.js
+++ cypress/integration/simple.spec.js
@@ -38,7 +38,7 @@ describe('allure report spec', function () {
         cy.url().should("include", "/uk");
     });
 
-    it('should be on DE site', function () {
+    it.only('should be on DE site', function () {
 
         // Allure Report
         cy.allure()
